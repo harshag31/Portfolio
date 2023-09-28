69bea2d1d3fa3f7d19cf0d59bf151ad893eea550
harshag31 <harshag3103@gmail.com>
2023-09-28 14:43:25 +0530

    navbar1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import a from 'next/link';
 import { useRouter } from 'next/router'
-import Logo from './Logo'
+import Logo from './Logo';
+import Linkedin from './icons'
+import {motion} from "framer-motion";
 
 const CustomLink=({href,title,className=""})=>{
 const router = useRouter();
-console.log(router)
 
   return(
-    <Link href={href} className={`${className} relative group`}>
+    <a href={href} className={`${className} relative group`}>
       {title}
-      <span className='h-[1px] inline-block w-0 bg-dark
+      <span className={`h-[1px] inline-block w-0 bg-dark
       absolute left-0 -bottom-0.5 
       group-hover:w-full transition-[width] ease duration-300
-
-      '>&nbsp;</span>
-    </Link>
+      ${router.asPath ===href ? 'w-full':'w-0'}
+      `}>&nbsp;</span>
+    </a>
   )
 }
 
@@ -29,8 +30,14 @@ const NavBar = () => {
     <CustomLink href="/projects" title="Projects" className='mx-4'/>
     <CustomLink href="/articles" title="Certificates" className='ml-4'/>
   </nav>
-  <nav>
-    <Link href="/" target={"_blank"}>T</Link>
+  <nav className="flex item-center justify-center flex-wrap">
+    <motion.a href="https://linkedin.com" target={"_blank"}
+    className='w-6 ml-3'
+    whileHover={{y:-2 }}
+    whileTap={{scale:0.9}}
+    >
+      <Linkedin/>
+    </motion.a>
   </nav>
   <div className='absolute left-[50%] top-2 translate-x-[-50%]'><Logo/></div>
 
