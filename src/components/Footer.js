cb7000a8fab451e6a4d04bae5e77615733eb1e00
harshag31 <harshag3103@gmail.com>
2023-09-30 19:56:17 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,13 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>``
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-1g'>
       <Layout className='py-8 flex items-center justify-between'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <div className='flex items-center'>
-        Build with <span className='text-primary text-2x1 px-1'>&#9825;</span>
-        by&nbsp;<Link href="">Harsha G</Link>
+        <Link href="/">Build with <span className='text-primary text-2x1 px-1'> &#x1F49B; </span>by&nbsp;Harsha G</Link>
         </div>
-        <Link href="mailto:harshag3103@gmail.com?subject=Your%20Subject&body=You%20can%20Start%20by%20Saying%20Hi%20!." target={"_blank"}>Say hello</Link>
+        <Link href="mailto:harshag3103@gmail.com?subject=Your%20Subject&body=You%20can%20Start%20by%20Saying%20Hi%20!." target={"_blank"} className='underline underline-offset-2'>Say hello &#128516;</Link>
       </Layout>
     </footer>
   )
