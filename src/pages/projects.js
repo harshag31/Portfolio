17cec0913d46d19c26ace46bbfb08c8b5aba543d
harshag31 <harshag3103@gmail.com>
2023-10-02 20:33:56 +0530

    lock resume

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,7 @@ import React from 'react'
 const projects = () => {
   return (
     <Layout className='pt-16'>
-            <div className='justify-center text-8xl text-center'>OOPS!&nbsp; &#128577; Under Develelopment</div>
+            <div className='justify-center p-28 text-8xl text-center'>OOPS!&nbsp; &#128577; Under Development</div>
     </Layout>
     
   )
